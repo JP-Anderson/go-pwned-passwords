94d17c386ac9888c8061a0366a9b012f6e64fca6
dahui58 <dahui58@hotmail.com>
2019-01-19 19:31:48 +0000

    Few more test cases

--- passwordprocessing/passwordprocessing_test.go
+++ passwordprocessing/passwordprocessing_test.go
@@ -11,6 +11,8 @@ var shaTestCases = []struct {
 	base64Sha string
 }{
 	{ "Password1", "70CCD9007338D6D81DD3B6271621B9CF9A97EA00" },
+	{ "5TronG-p45sw()rd", "2CAF0B7082B8CA0F41C815F14D6E216602784895" },
+	{ "letmein", "B7A875FC1EA228B9061041B7CEC4BD3C52AB3CE3" },
 }
 
 func TestStringToSha1(t *testing.T) {
